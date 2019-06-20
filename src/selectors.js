b61b8c1054f5b7ffdc4b97408098f5348a001c35
Marieth <mperezzevallos@gmail.com>
2019-06-20 17:43:03 -0500

    Add selector of dashboard

--- src/selectors.js
+++ src/selectors.js
@@ -1,2 +1,8 @@
 import React from "react";
 import { useSelector, shallowEqual } from "react-redux";
+
+function useResults() {
+  return useSelector(state => Object.values(state.results), shallowEqual);
+}
+
+export { useResults };
