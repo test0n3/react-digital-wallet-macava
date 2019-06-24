4658de2ac0c0d29d9c52302e64fe94d4f6a82fc2
Carlos A <cayala.w@gmail.com>
2019-06-24 16:50:06 -0500

    Merge with original project

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./reducer";
+<<<<<<< HEAD
 import offlineSync from "./middlewares/offline-sync";
+=======
+>>>>>>> f65741f6da484816fb9e18a82c7474e08acbf820
 import logger from "redux-logger";
 
 const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
