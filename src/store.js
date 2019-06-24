b7c1058f371be1e1d319e3a511fb125488e5f8bf
Carlos A <cayala.w@gmail.com>
2019-06-24 18:01:28 -0500

    Update application for PWA

--- src/store.js
+++ src/store.js
@@ -1,9 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import reducer from "./reducer";
-<<<<<<< HEAD
+import reducer, { initialState } from "./reducer";
 import offlineSync from "./middlewares/offline-sync";
-=======
->>>>>>> f65741f6da484816fb9e18a82c7474e08acbf820
 import logger from "redux-logger";
 
 const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -12,7 +9,9 @@ if (localStorage.getItem("version") !== process.env.REACT_APP_VERSION) {
   localStorage.removeItem("state");
 }
 
-const preloadedState = JSON.parse(localStorage.getItem("state") || "{}");
+const preloadedState = JSON.parse(
+  localStorage.getItem("state") || JSON.stringify(initialState)
+);
 
 const store = createStore(
   reducer,
